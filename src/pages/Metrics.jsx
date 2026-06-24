b2b228171e8bf0114144cc8478b8f12bc01c15fe
Demo User <demo@example.com>
2026-06-23 19:12:46 -0500

    Fix React import bug

--- src/pages/Metrics.jsx
+++ src/pages/Metrics.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useStore } from '../context/StoreContext';
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid,
